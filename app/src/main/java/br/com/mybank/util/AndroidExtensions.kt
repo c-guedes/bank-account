1d9b3441d0dffc69970c9c33153bf3de2523187e
Caique Guedes de Almeida e Silva <caique-guedes@hotmail.com>
2020-03-15 17:27:51 -0300

    included text view validator

--- app/src/main/java/br/com/mybank/util/AndroidExtensions.kt
+++ app/src/main/java/br/com/mybank/util/AndroidExtensions.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.os.Build
 import android.view.View
+import android.widget.TextView
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 
@@ -23,3 +24,20 @@ fun Double.formatValueToMonetary() =
     FORMAT_WITH_SYMBOL.decimalFormatSymbols.currencySymbol.plus(this)
 
 fun Context.getResColor(@ColorRes idColor: Int) = ContextCompat.getColor(this, idColor)
+
+fun TextView.validatePassword(): Boolean {
+    return if (PASSWORD_PATTERN.matches(text.toString())) {
+        true
+    } else {
+        error = "Password inválido"
+        false
+    }
+}
+
+fun TextView.validateCPF(): Boolean {
+    return CPF_PATTERN.matches(text.toString())
+}
+
+fun TextView.validateEmail(): Boolean {
+    return EMAIL_PATTERN.matches(text.toString())
+}
